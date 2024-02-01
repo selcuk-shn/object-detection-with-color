5d22b1d2002df9048503d47cbbe3da9fed916b9e
selcuk-shn <150268443+selcuk-shn@users.noreply.github.com>
2024-02-01 16:06:28 +0300

    Renk ile Nesne Tespiti.py

--- Renk ile Nesne Tespiti.py
+++ Renk ile Nesne Tespiti.py
@@ -87,29 +87,4 @@ while True:
      
     if cv2.waitKey(1) & 0xFF == ord('q'): break
 cap.release()
-cv2.destroyAllWindows()
-
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+cv2.destroyAllWindows()   
